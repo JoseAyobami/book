8c7959ee79f3f62625b81f51bf021d6771e117bc
Jose <ayenijoseph45@gmail.com>
2025-09-27 20:58:12 +0100

    modified setting.py

--- app/setting.py
+++ app/setting.py
@@ -9,9 +9,6 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
     refresh_token_expire_days: int
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
     
 
     # App
