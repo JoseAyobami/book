ddba015dffd8c6d80e6e87989616dde143a941cd
Jose <ayenijoseph45@gmail.com>
2025-09-27 21:37:07 +0100

    modified setting file

--- app/setting.py
+++ app/setting.py
@@ -2,13 +2,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Database
-    database_url: str
-
-    
+    database_url: str    
     secret_key: str
     algorithm: str
-    access_token_expire_minutes: int
-    refresh_token_expire_days: int
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 7
     
 
     # App
