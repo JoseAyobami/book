1bc901ea5dd47aeeec894c46ddd887f520c0a5de
Jose <ayenijoseph45@gmail.com>
2025-10-03 20:15:20 +0100

    fix database url

--- app/database.py
+++ app/database.py
@@ -4,15 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.setting import settings
 
-if settings.environment == "production":
-    DATABASE_URL = os.getenv("DATABASE_URL_RENDER")
-else:
-    DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
 
-if not DATABASE_URL:
-    DATABASE_URL = settings.database_url  
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
